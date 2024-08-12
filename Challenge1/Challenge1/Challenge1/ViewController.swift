8d61e18aac24074d37622fa71013194320106436
Hamza <mozamiza@gmail.com>
2024-08-12 13:16:36 +0300

    version 1 of challenge app

--- Challenge1/Challenge1/Challenge1/ViewController.swift
+++ Challenge1/Challenge1/Challenge1/ViewController.swift
@@ -13,10 +13,33 @@ var shoppingList = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //added item add button
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
         
-        // Do any additional setup after loading the view.
+        
+        
     }
-
+    
+    @objc func addItem(){
+        let ac = UIAlertController(title: "Enter your item to the shopping list", message: nil, preferredStyle: .alert)
+        ac.addTextField()// add text field to alert
+        
+        let confirmAction = UIAlertAction(title: "Confirm", style: .default){
+            [weak self, weak ac] action in
+            guard let shoppingItem = ac?.textFields?[0].text else {return}
+            self?.submit(shoppingItem)
+        }
+        ac.addAction(confirmAction)
+        present (ac, animated: true)
+    }
+    func submit(_ shoppingItem: String){
+        shoppingList.insert(shoppingItem, at: 0)
+        
+        let indexPath = IndexPath(row: 0 , section: 0)
+        
+        tableView.insertRows(at: [indexPath], with: .automatic)
+        return
+    }
 
 }
 
