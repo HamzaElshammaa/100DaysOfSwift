13eb0c1d60cce62e9be0a188d795974267b35698
Hamza <mozamiza@gmail.com>
2024-08-12 12:43:43 +0300

    started on challenge #1

---
+++ Challenge1/Challenge1/Challenge1/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Challenge1
+//
+//  Created by Hamza Hatem on 12/08/2024.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+var shoppingList = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
